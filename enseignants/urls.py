475a2c3ab76af81e0881066bc4416367ecd0a7ef
Wilbert Saint-Louis <saintlouiswilbert374@gmail.com>
2025-10-15 19:45:10 -0400

    modifier_enseignant

--- enseignants/urls.py
+++ enseignants/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'enseignants'
 
 urlpatterns = [
-    path('', views.enseignants, name='enseignants'),
+    path('enseignants', views.enseignants, name='enseignants'),
+    path('ajouter_enseignant', views.ajouter_enseignants, name='ajouter_enseignant'),
+    path('modifier_enseignants', views.modifier_enseignants, name='modifier_enseignant'),
 ]
--- enseignants/views.py
+++ enseignants/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def enseignants(request):
     return render(request, 'enseignants/enseignants.html')
+
+
+def ajouter_enseignants(request):
+    return render(request, 'enseignants/ajouter_enseignant.html')
+
+
+def modifier_enseignants(request):
+    return render(request, 'enseignants/modifier_enseignant.html')
