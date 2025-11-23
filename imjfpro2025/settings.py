1fb5d78d8f97d72a95fe9e37081a0c56d142a1f3
Trotander <pa6832474@gmail.com>
2025-11-23 14:18:31 -0500

    modifikasyon modul yo 23  novanb

--- imjfpro2025/settings.py
+++ imjfpro2025/settings.py
@@ -173,9 +173,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': 'uSqQvJQqcAVXMknyUcAyKyqgYXsMOqts',
+        'PASSWORD': 'WrspyRaJxoIuUOMUkFedxcGCQkTNFYrC',
         'HOST': 'interchange.proxy.rlwy.net',
-        'PORT': '51525',
+        'PORT': '18186',
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
